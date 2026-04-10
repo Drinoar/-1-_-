100cc68c7615d0f3e20de6092feff1d819556811
Drinoar <80521836+Drinoar@users.noreply.github.com>
2026-04-10 12:51:39 +0300

    додано фікстуру та параметризацію

--- мкр1сд.py
+++ мкр1сд.py
@@ -1,3 +1,6 @@
+import pytest
+from src.main import add, subtract, multiply, divide
+
 def add(a, b):
     return a + b
 
@@ -12,3 +15,22 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Division by zero")
     return a / b
+
+
+
+@pytest.fixture
+def numbers():
+    return 10, 5
+# 🔹 TEST + FIXTURE
+def test_subtract(numbers):
+    a, b = numbers
+    assert subtract(a, b) == 5
+
+
+@pytest.mark.parametrize("a,b,expected", [
+    (2, 3, 6),
+    (5, 0, 0),
+    (-2, 4, -8)
+])
+def test_multiply(a, b, expected):
+    assert multiply(a, b) == expected
